35a8e9d1b64ff9ff24ca6e8755b6263ea5c47eee
Luke Storry <luke.storry@ghyston.com>
2020-02-12 17:43:21 +0000

    added simple list of food items from firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,122 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+final dummySnapshot = [
+  {"name": "Filip", "votes": 15},
+  {"name": "Abraham", "votes": 14},
+  {"name": "Richard", "votes": 11},
+  {"name": "Ike", "votes": 10},
+  {"name": "Justin", "votes": 1},
+];
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Eat Me',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(title: Text('Eat Me')),
+      body: _buildBody(context),
     );
   }
+
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<DocumentSnapshot>(
+      // todo set custom document
+      stream: Firestore.instance
+          .collection('kitchens')
+          .document('kitch01') // TODO kitchen selection per-user
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+
+        final kitchen = Kitchen.fromSnapshot(snapshot);
+        // TODO use kitchen name too?
+        return _buildFoodList(context, kitchen.foods);
+      },
+    );
+  }
+
+  Widget _buildFoodList(BuildContext context, List<Food> foods) {
+    print("building FoodList for " + foods.toString());
+
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: foods
+          .map((data) => _buildFoodListItem(context, data))
+          .toList(), //TODO sorting?
+    );
+  }
+
+  Widget _buildFoodListItem(BuildContext context, Food food) {
+    print("building FoodListItem for " + food.toString());
+    final daysUntilExpiry = food.expiry.difference(DateTime.now()).inDays;
+    final expiryWarningColor = daysUntilExpiry < 1
+        ? Colors.red
+        : daysUntilExpiry <= 2 ? Colors.amber : Colors.grey;
+    return Padding(
+      key: ValueKey(food.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: expiryWarningColor),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(food.name),
+          trailing: Text(food.expiry.toString()),
+          onTap: () => print(food), // TODO open modal
+        ),
+      ),
+    );
+  }
+}
+
+class Kitchen {
+  final String name;
+  final List<Food> foods;
+
+// final DocumentReference reference; // TODO do we need to set some reference for editablility?
+
+  Kitchen.fromSnapshot(snapshot)
+      : assert(snapshot.data['name'] != null),
+        name = snapshot.data['name'],
+        foods = snapshot.data['foods']
+            .map<Food>((data) => Food.fromSnapshot(data))
+            .toList()
+              ..sort((Food a, Food b) => a.expiry.compareTo(b.expiry));
+
+  @override
+  String toString() => "Kitchen<$name>";
+}
+
+class Food {
+  final String name;
+  final DateTime expiry;
+
+  Food.fromSnapshot(data)
+      : assert(data['name'] != null),
+        name = data['name'],
+        expiry = new DateTime.fromMicrosecondsSinceEpoch(
+            data['expiry'].microsecondsSinceEpoch);
+
+  @override
+  String toString() => "FoodItem<$name:$expiry>";
 }
